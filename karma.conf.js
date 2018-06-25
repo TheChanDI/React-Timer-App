36cc2c5233c9a97ff1576fcc6ff328a216adea9a
thechandi <chandiprakash16@gmail.com>
2018-06-25 22:32:10 +0545

    update karma file

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config) {
     config.set({
-        browsers: ['chrome'],
+        browsers: ['Chrome'],
         singleRun: true,
         frameworks: ['mocha'],
         files: ['app/tests/**/*.test.jsx'],
